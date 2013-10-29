2ae5f4a78dba33871d53e67dc5a03c215d6d1b3f
anvaka <anvaka@gmail.com>
2013-10-29 01:20:16 -0700

    Added transform stream for vertices to vertices

--- test/nodes.js
+++ test/nodes.js
@@ -9,10 +9,8 @@ test('Get all nodes', function (t) {
   g = shremlin(pathGraph);
 
   g.V().on('data', function (vertex) {
-    t.ok(vertex);
+    t.ok(vertex, "Vertex should be present");
   });
-
-  t.end();
 });
 
 test('Get specific node', function(t) {
@@ -23,8 +21,38 @@ test('Get specific node', function(t) {
   g = shremlin(pathGraph);
 
   g.V(startNodeId).on('data', function (vertex) {
-    t.equal(vertex.id, startNodeId, "Unexpected start node");
+    t.equal(vertex.id, startNodeId, "Start vertext should be valid");
   });
+});
 
-  t.end();
+test('Get adjacent vertices', function(t) {
+  var graphSize = 5,
+      graph = generators.complete(graphSize),
+      startNodeId = 0;
+
+  t.plan(graphSize - 1);
+
+  g = shremlin(graph);
+  g.V(startNodeId)
+    .out()
+    .on('data', function (vertex) {
+      t.notEqual(vertex.id, startNodeId, "None of the adjacent vertices should be equal to start node");
+    });
+});
+
+test('Get adjacent of adjacent', function(t) {
+  var graphSize = 5,
+      graph = generators.path(graphSize),
+      startNodeId = 0;
+
+  t.plan(1);
+
+  g = shremlin(graph);
+  debugger;
+  g.V(startNodeId)
+    .out()
+    .out()
+    .on('data', function (vertex) {
+      t.equal(vertex.id, 2, "Adjacent node should be 2");
+    });
 });
