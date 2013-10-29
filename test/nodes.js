baf41c8fd5e82ea2be63e99cac429ff5259b808d
anvaka <anvaka@gmail.com>
2013-10-29 01:59:52 -0700

    Added stream extension for common syntactic sugar extensions.

--- test/nodes.js
+++ test/nodes.js
@@ -1,6 +1,7 @@
 var test = require('tap').test,
     shremlin = require('..'),
-    generators = require('ngraph.generators');
+    generators = require('ngraph.generators'),
+    createGraph = require('ngraph.graph');
 
 test('Get all nodes', function (t) {
   t.plan(10);
@@ -25,33 +26,88 @@ test('Get specific node', function(t) {
   });
 });
 
-test('Get adjacent vertices', function(t) {
-  var graphSize = 5,
-      graph = generators.complete(graphSize),
-      startNodeId = 0;
-
-  t.plan(graphSize - 1);
-
+test('Test VerticesToVertices stream', function(t) {
+  // this is our test graph:
+  // 1 -> 2 <- 3
+  //      v
+  // 6 <- 4 -> 5
+  var graph = createGraph();
+  graph.addLink(1, 2); graph.addLink(3, 2);
+  graph.addLink(2, 4); graph.addLink(4, 5); graph.addLink(4, 6);
   g = shremlin(graph);
-  g.V(startNodeId)
-    .out()
-    .on('data', function (vertex) {
-      t.notEqual(vertex.id, startNodeId, "None of the adjacent vertices should be equal to start node");
-    });
+
+  t.test('4 > out', function (t) {
+    t.plan(2); // node 4 has only two out edges
+    g.V(4)
+      .out()
+      .on('data', function (vertex) {
+        t.ok(vertex.id === 6 || vertex.id === 5, "Vertex 4 connected to correct neighbors");
+      });
+  });
+
+  t.test('2 > out | out', function (t) {
+    t.plan(2);
+    g.V(2)
+      .out().out()
+      .on('data', function (vertex) {
+        t.ok(vertex.id === 6 || vertex.id === 5, "Vertex 4 connected to correct neighbors");
+      });
+  });
+
+  t.test('4 > in | out', function (t) {
+    t.plan(1);
+    g.V(4)
+      .in().out()
+      .on('data', function (vertex) {
+        t.ok(vertex.id === 4, "Vertex 4 should be the only out node of vertex 2");
+      });
+  });
+
+  t.test('2 > out | in', function (t) {
+    t.plan(1);
+    g.V(2)
+      .out().in()
+      .on('data', function (vertex) {
+        t.ok(vertex.id === 2, "Vertex 2 should be the only in node of vertex 4");
+      });
+  });
+
+  t.test('2 > both', function (t) {
+    t.plan(3);
+    g.V(2)
+      .both()
+      .on('data', function (vertex) {
+        t.ok([1, 3, 4].indexOf(vertex.id) !== -1, "Vertex 2 should be connected with 1, 3 and 4");
+      });
+  });
+
+  t.test('2 > both | in', function (t) {
+    t.plan(1);
+    g.V(2)
+      .both().in()
+      .on('data', function (vertex) {
+        t.equal(vertex.id, 2, "Vertex 2 should be the only possible in");
+      });
+  });
+  // Graph reminder:
+  // 1 -> 2 <- 3
+  //      v
+  // 6 <- 4 -> 5
+  t.test('2 > both | out', function (t) {
+    t.plan(4);
+    g.V(2)
+      .both().out()
+      .on('data', function (vertex) {
+        t.ok([2, 6, 5].indexOf(vertex.id) !== -1);
+      });
+  });
+
+  t.test('2 > in | in', function (t) {
+    g.V(2)
+      .in().in()
+      .on('data', function (vertex) {
+        t.fail("There is no 'in' nodes for 1 and 3");
+      }).on('end', function() { t.end(); });
+  });
 });
 
-test('Get adjacent of adjacent', function(t) {
-  var graphSize = 5,
-      graph = generators.path(graphSize),
-      startNodeId = 0;
-
-  t.plan(1);
-
-  g = shremlin(graph);
-  g.V(startNodeId)
-    .out()
-    .out()
-    .on('data', function (vertex) {
-      t.equal(vertex.id, 2, "Adjacent node should be 2");
-    });
-});
