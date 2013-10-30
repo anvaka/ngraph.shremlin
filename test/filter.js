1f46ba3df0139a23fc6e66ddd70155d3dd39c2d0
anvaka <anvaka@gmail.com>
2013-10-30 00:51:08 -0700

    Updated filter to match against data attribute

--- test/filter.js
+++ test/filter.js
@@ -1,14 +1,22 @@
 var test = require('tap').test,
     createFilter = require('../lib/filterExpression');
 
+// in ngraph.* all links and nodes store associated data within
+// 'data' proprerty. This test supposed to verify maches against data properties
 test('Can match simple types', function (t) {
   var matchHello = createFilter('hello');
-  t.ok(matchHello('hello'), 'Should match strings');
-  t.notOk(matchHello('Hello'), 'Should be case sensitive');
-  t.notOk(matchHello('hello world'), 'Should not do partial match');
-  t.notOk(matchHello(''), 'Should not match with empty string');
-  t.notOk(matchHello(1), 'Should not match with non strings');
-  t.notOk(matchHello('world'), 'Should not match when there is no match');
+  t.ok(matchHello({data: 'hello'}), 'Should match strings');
+  t.notOk(matchHello({data: 'Hello'}), 'Should be case sensitive');
+  t.notOk(matchHello({data: 'hello world'}), 'Should not do partial match');
+  t.notOk(matchHello({data: ''}), 'Should not match with empty string');
+  t.notOk(matchHello({data: 1}), 'Should not match with non strings');
+  t.notOk(matchHello({data: 'world'}), 'Should not match when there is no match');
   t.notOk(matchHello(), 'Should not match with undefined');
+
+  var matchNumber = createFilter(42);
+  t.ok(matchNumber({ data: 42 }), 'Should match strings');
+  t.notOk(matchNumber({ data: 1 }), 'Should not match with non strings');
+  t.notOk(matchNumber(), 'Should not match with undefined');
   t.end();
 });
+
