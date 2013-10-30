a5613304ec161e6829975fb2ec42be86640b45e8
anvaka <anvaka@gmail.com>
2013-10-30 01:27:23 -0700

    Split vertices and verticesToVertices tests

---
+++ test/vertices.js
@@ -0,0 +1,26 @@
+var test = require('tap').test,
+    shremlin = require('..'),
+    generators = require('ngraph.generators');
+
+test('Get all nodes', function (t) {
+  t.plan(10);
+
+  var pathGraph = generators.path(10);
+  g = shremlin(pathGraph);
+
+  g.V().on('data', function (vertex) {
+    t.ok(vertex, "Vertex should be present");
+  });
+});
+
+test('Get specific node', function(t) {
+  t.plan(1);
+
+  var pathGraph = generators.path(10),
+      startNodeId = 0;
+  g = shremlin(pathGraph);
+
+  g.V(startNodeId).on('data', function (vertex) {
+    t.equal(vertex.id, startNodeId, "Start vertext should be valid");
+  });
+});
--- test/verticesToVerticesStream.js
+++ test/verticesToVerticesStream.js
@@ -1,30 +1,6 @@
 var test = require('tap').test,
     shremlin = require('..'),
-    generators = require('ngraph.generators'),
     createGraph = require('ngraph.graph');
-
-test('Get all nodes', function (t) {
-  t.plan(10);
-
-  var pathGraph = generators.path(10);
-  g = shremlin(pathGraph);
-
-  g.V().on('data', function (vertex) {
-    t.ok(vertex, "Vertex should be present");
-  });
-});
-
-test('Get specific node', function(t) {
-  t.plan(1);
-
-  var pathGraph = generators.path(10),
-      startNodeId = 0;
-  g = shremlin(pathGraph);
-
-  g.V(startNodeId).on('data', function (vertex) {
-    t.equal(vertex.id, startNodeId, "Start vertext should be valid");
-  });
-});
 
 test('Test VerticesToVertices stream', function(t) {
   // this is our test graph:
