0a96ca4e792f0d91925ffdcce0e8f5693c9a08a3
anvaka <anvaka@gmail.com>
2013-10-31 00:00:49 -0700

    Added graph of gods builder

---
+++ test/graphOfGods.js
@@ -0,0 +1,70 @@
+// A test graph from gremlin:
+// https://raw.github.com/wiki/thinkaurelius/titan/images/graph-of-the-gods-2.png
+
+module.exports = createGraphOfGods;
+
+function createGraphOfGods() {
+  var graph = require('ngraph.graph')();
+
+  graph.addNode('hercules', { age: 30, type: 'demigod'});
+  graph.addNode('jupiter', { age: 5000, type: 'god'});
+  graph.addNode('alcmene', { age: 45, type: 'human'});
+  graph.addNode('saturn', { age: 10000, type: 'titan'});
+  graph.addNode('pluto', { age: 4000, type: 'god'});
+  graph.addNode('neptune', { age: 4500, type: 'god'});
+
+  graph.addNode('nemean', { type: 'monster'});
+  graph.addNode('hydra', { type: 'monster'});
+  graph.addNode('cerberus', { type: 'monster'});
+
+  graph.addNode('sky', { type: 'location'});
+  graph.addNode('sea', { type: 'location'});
+  graph.addNode('tartarus', { type: 'location'});
+
+  // done with nodes, add edges:
+  grpah.addLink('hercules', 'nemean', {
+    action: 'battled',
+    time: 1,
+    place: [38.1, 23.7]
+  });
+  grpah.addLink('hercules', 'hydra', {
+    action: 'battled',
+    time: 2,
+    place: [37.7, 23.9]
+  });
+  grpah.addLink('hercules', 'cerberus', {
+    action: 'battled',
+    time: 12,
+    place: [39, 22]
+  });
+  graph.addLink('hercules', 'alcmene', 'mother');
+  graph.addLink('hercules', 'jupiter', 'father');
+
+  graph.addLink('cerberus', 'tartarus', { action: 'lives' });
+  graph.addLink('pluto', 'tartarus', {
+   action: 'lives',
+   reason: 'no fear of death'
+  });
+  graph.addLink('pluto', 'jupiter', 'brother');
+  graph.addLink('jupiter', 'pluto', 'brother');
+
+  graph.addLink('pluto', 'neptune', 'brother');
+  graph.addLink('neptune', 'pluto', 'brother');
+
+  graph.addLink('jupiter', 'neptune', 'brother');
+  graph.addLink('neptune', 'jupiter', 'brother');
+
+
+  graph.addLink('jupiter', 'saturn', 'father');
+  graph.addLink('jupiter', 'sky', {
+    action: 'lives',
+    reason: 'loves fresh breezes'
+  });
+
+  graph.addLink('neptune', 'sea', {
+    action: 'lives',
+    reason: 'loves waves'
+  });
+
+  return graph;
+}
