c05643e6d574b2cdb3602c0dfc07a01378056639
anvaka <anvaka@gmail.com>
2013-11-04 23:55:40 -0800

    Updated folders structure

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 module.exports = function(graph) {
   return {
     V: function createVertexStream(startFrom) {
-      var VertexStream = require('./lib/vertexStream');
+      var VertexStream = require('./lib/streams/vertexStream');
       return new VertexStream(graph, startFrom);
     }
   };
