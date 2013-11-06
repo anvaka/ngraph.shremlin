d93557f84f59fe01b35c84c99c787dfdeca26d5f
anvaka <anvaka@gmail.com>
2013-11-06 01:54:13 -0800

    Added Path pipe to track pipelines

--- test/pathStream.js
+++ test/pathStream.js
@@ -1,4 +1,3 @@
-  /*
 var gods = require('./data/graphOfGodsData')(),
     g = require('..')(gods),
     test = require('tap').test;
@@ -9,7 +8,7 @@ test('Get path from Hercules', function (t) {
     g.V('hercules')
      .out()
      .path()
-     .on('data', function(path) {
+     .forEach(function(path) {
        t.equal(path.length, 2, 'All out paths should be 2 item long');
        t.equal(path[0].id, 'hercules', 'All paths starts at Hercules');
      });
@@ -20,7 +19,7 @@ test('Get path from Hercules', function (t) {
     g.V('hercules')
      .out('father')
      .path()
-     .on('data', function (path) {
+     .forEach(function (path) {
        t.equal(path[1].id, 'jupiter', 'Father of Hercules is Jupiter');
      });
   });
@@ -29,7 +28,7 @@ test('Get path from Hercules', function (t) {
     g.V('hercules')
      .out('father').out('father')
      .path()
-     .on('data', function (path) {
+     .forEach(function (path) {
        t.equal(path[2].id, 'saturn', 'Grandfather of Hercules is Saturn');
      });
   });
@@ -39,7 +38,7 @@ test('Get path from Hercules', function (t) {
     g.V('hercules')
      .outE('father').inV()
      .path()
-     .on('data', function (path) {
+     .forEach(function (path) {
        t.equal(path[1].fromId, 'hercules', 'Start node should be Hercules');
        t.equal(path[1].toId, 'jupiter', 'End node should be Jupiter');
        t.equal(path[2].id, 'jupiter', 'Father of Hercules is Jupiter');
@@ -49,18 +48,20 @@ test('Get path from Hercules', function (t) {
   // hercules > father > brother > pet > {action: 'battled'}
   t.test('Who battled pet of Hercules father\'s brother?', function (t) {
     var battled = {action: 'battled'};
-    t.plan(1);
+    t.plan(5);
 
     g.V('hercules')
      .out('father')
      .out('brother')
      .out('pet')
+     .in(battled)
      .path()
-     .on('data', function (path) {
-       debugger;
-       t.equal(path[5].id, 'hercules', 'It was Hercules');
-
-     })
+     .forEach(function (path) {
+       t.equal(path[0].id, 'hercules', 'Started from Hercules');
+       t.equal(path[1].id, 'jupiter', 'Went to Jupiter');
+       t.equal(path[2].id, 'pluto', 'Brother was Pluto');
+       t.equal(path[3].id, 'cerberus', 'Pluto\'s pet was Cerberus');
+       t.equal(path[4].id, 'hercules', 'And Hercules batteled Cerberus');
+     });
   })
 });
-  */
