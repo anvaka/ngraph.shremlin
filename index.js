710a96de96d03706d3c48e25d207b9bb45cde461
anvaka <anvaka@gmail.com>
2013-11-05 20:40:26 -0800

    Renamed VertexStream to VertexPipe

--- index.js
+++ index.js
@@ -3,8 +3,12 @@
 module.exports = function(graph) {
   return {
     V: function createVertexStream(startFrom) {
-      var VertexStream = require('./lib/streams/vertexStream');
-      return new VertexStream(graph, startFrom);
+      var VertexPipe = require('./lib/streams/vertexPipe');
+      var VertexIterator = require('./lib/utils/vertexIterator');
+
+      var vertexPipe = new VertexPipe(graph);
+      vertexPipe.setStarts(new VertexIterator(graph, startFrom));
+      return vertexPipe;
     }
   };
 };
