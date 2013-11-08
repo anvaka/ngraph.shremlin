0bedcffcff8605e0f4b69432bd9b82ab2d0e5647
anvaka <anvaka@gmail.com>
2013-11-08 01:23:57 -0800

    Generated documentation for ff48e9bd43f9d43cd40fc1b3ef3520bfedc70a1b

--- assets/behavior.js
+++ assets/behavior.js
@@ -51,7 +51,17 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
         "projectPath": "lib/basePipe.js",
         "targetPath": "basePipe",
         "pageTitle": "basePipe",
-        "title": "basePipe"
+        "firstHeader": {
+          "type": "heading",
+          "data": {
+            "level": 1,
+            "title": "Base Pipe",
+            "slug": "base-pipe",
+            "isFileHeader": true
+          },
+          "depth": 1
+        },
+        "title": "Base Pipe"
       },
       "depth": 1,
       "outline": []
